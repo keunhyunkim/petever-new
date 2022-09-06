85be7139acf2e3dfb1d6df36465db5a69c584a50
Brissa yu <jungin641@gmail.com>
2022-09-06 23:23:59 +0900

    MOD logic of check permission

--- Android/PetEver/app/src/main/java/com/example/petever/oobe/MainActivity.java
+++ Android/PetEver/app/src/main/java/com/example/petever/oobe/MainActivity.java
@@ -6,19 +6,18 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
 
 import com.example.petever.R;
 
 public class MainActivity extends AppCompatActivity {
-    static final int PERMISSIONS_REQUEST_CODE = 1000;
-    String[] PERMISSIONS = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,40 +27,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        Button btn_camera = (Button) findViewById(R.id.btn_camera);
+        Button btn_camera = findViewById(R.id.btn_camera);
         btn_camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getCameraPermission();
-            }
-        });
-    }
-
-    private void getCameraPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // check permission
-            if (!hasPermissions(PERMISSIONS)) {
-                requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
-            } else {
                 Intent intent = new Intent(MainActivity.this, CameraActivity.class);
                 startActivity(intent);
                 //finish();
             }
-        }
+        });
     }
-
-    private boolean hasPermissions(String[] permissions) {
-        int result;
-
-        for (String perms : permissions) {
-            result = ContextCompat.checkSelfPermission(this, perms);
-            if (result == PackageManager.PERMISSION_DENIED) {
-                return false;
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Log.d("PERM", "퍼미션 요청");
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
             }
         }
-
-        // all permission has got
-        return true;
     }
 
 
