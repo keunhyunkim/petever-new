45841000cc820ff8932339a8763fe51d1e1bdbf6
jeongchanKim <jc_.kim@samsung.com>
2022-09-07 18:46:31 +0900

    app/petever/oobe/CameraActivity.java : Change to show the Toast for breed classification result
    It is much better to show the breed classificatio result with Toast,
    instead of Log.d.
    Then it is easy to check the result.
    
    Signed-off-by: jeongchanKim <jc_.kim@samsung.com>

--- Android/PetEver/app/src/main/java/com/example/petever/oobe/CameraActivity.java
+++ Android/PetEver/app/src/main/java/com/example/petever/oobe/CameraActivity.java
@@ -10,7 +10,6 @@ import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.params.StreamConfigurationMap;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -31,7 +30,6 @@ import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
@@ -148,7 +146,16 @@ public class CameraActivity extends AppCompatActivity {
                                 MediaStore.Images.Media.insertImage(getContentResolver(), btmImg,  dataFormat.format(new Date()) + ".png", "taken by petEver");
 
                                 String breed = inf.runBreedClassification(btmImg);
+                                Handler mHandler = new Handler(Looper.getMainLooper());
                                 Log.d("RESULT", "RESULT : " + breed);
+                                mHandler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(CameraActivity.this, "Breed : " + breed,
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }, 0);
+
                                 runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
