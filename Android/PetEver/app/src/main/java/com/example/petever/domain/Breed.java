5c2c7170d9a46b68fd0afbcb030362a3a3a37b58
jungining <jungin641@gmail.com>
2022-09-21 10:22:05 +0900

    MOD code desc of Breed Enum

--- Android/PetEver/app/src/main/java/com/example/petever/domain/Breed.java
+++ Android/PetEver/app/src/main/java/com/example/petever/domain/Breed.java
@@ -8,22 +8,26 @@ import java.util.Map;
 public enum Breed {
     // 동물 코드 + 종 코드 + 특징 코드
 
-    // 강아지 : 10
-    // 고양이 : 11
-
-    // 포메 : 001
-    // 말티즈 : 002
-
-    // long컷 : 1
-    // 곰돌이컷 : 2
-    // 말티즈 기본 : 1
+    // AABBBC
+    // AA : 동물 코드 (강아지 : 10, 고양이 11)
+    // BBB : 종 코드 (강아지-포메 : 001, 말티즈 : 002)
+    // C : 특징 코드
+    // |   동물    |        종          |       특징       |
+    // ---------------------------------------------------
+    // | 강아지(10) | 포메라니안(001)     |  long컷(1)       |
+    // |           |                   | 곰돌이컷(2)       |
+    // ---------------------------------------------------
+    // | 강아지(10) | 말티즈(002)        | 기본컷(1)         |
+    // ---------------------------------------------------
+    // | 고양이(11) |                   |                  |
+    // ---------------------------------------------------
 
     MALTESE(100021, "MALTESE", R.string.breed_maltese), POME_LONG(100011, "POME_LONG", R.string.breed_pome), POME_SHORT(100012, "POME_SHORT", R.string.breed_pome);
     private int code;
     private String name;
     private int bubbleText;
 
-    Breed(int code, String name, int bubbleText){
+    Breed(int code, String name, int bubbleText) {
         this.code = code;
         this.name = name;
         this.bubbleText = bubbleText;
