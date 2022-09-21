67c1d8af005869d0f4464d284a83c846624ee479
hjgpwn <hjgpwn@naver.com>
2022-09-21 11:16:33 +0900

    ReferenceCodes/annotation Define variable which indicates the breed name and file path

--- ReferenceCodes/annotation/image_annotation.py
+++ ReferenceCodes/annotation/image_annotation.py
@@ -2,19 +2,24 @@ import cv2
 import os
 from pascal_voc_writer import Writer
 
+image_file_path = './input/pome_short_'
+annotation_file_path = './annotation/pome_short_'
+breed_name = 'pomeranian'
+
 #print(os.getcwd())
 #파일 읽어오는 부분
 for i in range(1,5):
-    im = cv2.imread('./input/pome_short_' + str(i) + '.jpg')
+    im = cv2.imread(image_file_path + str(i) + '.jpg')
     #print(type(im))
     #print(im.shape)
     #print(type(im.shape))
+	
 #크기 설정하는 부분
     height, width, depth = im.shape
     #print(height, width, depth)
 #xml 파일로 annotation 저장
-    writer = Writer('./annotation/pome_short_' + str(i) + '.jpg', width, height, depth)
-    writer.addObject('pomeranian',193, 47, 340, 150)
+    writer = Writer(annotation_file_path + str(i) + '.jpg', width, height, depth)
+    writer.addObject(breed_name ,193, 47, 340, 150)
 
-    writer.save('./annotation/pome_short_' + str(i))
+    writer.save(annotation_file_path + str(i))
 
