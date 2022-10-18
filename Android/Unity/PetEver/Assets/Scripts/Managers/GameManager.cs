8c371a1110c7eb0094e21fa91b7efe6593a637c7
jungining <jungin641@gmail.com>
2022-10-18 17:38:08 +0900

    Get breed info from android

--- Android/Unity/PetEver/Assets/Scripts/Managers/GameManager.cs
+++ Android/Unity/PetEver/Assets/Scripts/Managers/GameManager.cs
@@ -9,12 +9,22 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
+
+            AndroidJavaObject intent = currentActivity.Call<AndroidJavaObject>("getIntent");
+
+            String arguments = intent.Call<string>("getStringExtra", "breed");
+
+            Debug.Log("[breed] arguments : " + arguments);
+        }
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
