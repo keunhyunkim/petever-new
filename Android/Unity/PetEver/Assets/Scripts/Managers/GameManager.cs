2eff2d6890cb0b970eb537063f3ff0501c733efe
jungining <jungin641@gmail.com>
2022-10-19 23:00:12 +0900

    CREATE dog 3d model dynamically by breed information from android

--- Android/Unity/PetEver/Assets/Scripts/Managers/GameManager.cs
+++ Android/Unity/PetEver/Assets/Scripts/Managers/GameManager.cs
@@ -13,20 +13,49 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        try
+        Vector3 dogScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
         {
-            using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            String breed = "";
+            GameObject dogPrefab;
+            try
             {
                 activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
 
                 AndroidJavaObject intent = activityContext.Call<AndroidJavaObject>("getIntent");
 
-                String breed = intent.Call<String>("getStringExtra", "breed");
+                breed = intent.Call<String>("getStringExtra", "breed");
                 String petName = intent.Call<String>("getStringExtra", "petname");
 
                 Debug.Log("[intent data] arguments : " + breed);
                 Debug.Log("[intent data] arguments : " + petName);
             }
+            catch (Exception e)
+            {
+                Debug.Log("GameManager Exception : " + e.ToString());
+            }
+
+            switch (breed)
+            {
+                case "POME_LONG":
+                    dogPrefab = pomeLongPrefab;
+                    break;
+                case "POME_SHORT":
+                    dogPrefab = pomeShortPrefab;
+                    break;
+                case "MALTESE":
+                    dogPrefab = maltesePrefab;
+                    break;
+                default:
+                    dogPrefab = pomeLongPrefab;
+                    break;
+            }
+
+            GameObject dog = Instantiate(dogPrefab, GameObject.Find("Object Parent").transform) as GameObject;
+            dog.transform.localScale = dogScale;
+        }
+    }
         }
         catch (Exception e)
         {
