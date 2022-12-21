fbf7f9ee2c1743eb97085ba1785510c9e5fef48c
jeongchanKim <jc_.kim@samsung.com>
2022-12-21 13:51:21 +0900

    Unity/PetEver : Add new scene manager
    When coliision with a house happens, change the scene to newScene.
    [To Do]
    1. Set the UserCharacter not to fall
    2. Add move-back to original scene
    
    Signed-off-by: jeongchanKim <jc_.kim@samsung.com>

---
+++ Unity/PetEver/Assets/02.Scripts/EnterHome.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class EnterHome : MonoBehaviour
+{
+    
+
+    private bool isEntered = false;
+    private Vector3 m_currentDirection = Vector3.zero;
+
+   
+    private void Awake()
+    {
+      
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       if (isEntered == false) {      
+        /*
+        * LoadSceneMode.Single  : 모든 씬을 닫고, 새로운 씬을 불러온다. 
+        *  
+        * LoadSceneMode.Additive : 현재 씬에 추가적으로 씬을 불러온다.
+        */
+        Debug.Log("New Scene Entered!!!");
+        SceneManager.LoadScene("newScene", LoadSceneMode.Single); 
+        //SceneManager.LoadScene("SceneName", LoadSceneMode.Additive);
+       }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+       
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+       
+    }
+
+    private void Update()
+    {
+       
+    }
+
+}
