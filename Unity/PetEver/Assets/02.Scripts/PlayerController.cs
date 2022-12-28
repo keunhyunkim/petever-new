53cde9f8511cdcb11b84072497e91978bfb4e279
keunhyunkim <keunhyun12@gmail.com>
2022-12-28 16:56:25 +0900

    joystick 변경(사용자 input 과 player이동 나눔) 포메라니안 long gray 캐릭터 씬에 추가

--- Unity/PetEver/Assets/02.Scripts/PlayerController.cs
+++ Unity/PetEver/Assets/02.Scripts/PlayerController.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
+
+// reference: https://scvtwo.tistory.com/111
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float playerSpeed = 5.0f; // Player character speed
+    PlayerInput inputValue;  
+    Vector3 move;
+
+    GameObject Player; 
+    
+
     void Start()
     {
-        
+        Player = GameObject.Find("Man");
+        inputValue = GameObject.Find("JoystickBGD").GetComponent<PlayerInput>(); // get joystick input data from 'PlayerInput' class 
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        move = new Vector3(inputValue.joystick_x * playerSpeed * Time.deltaTime, 0f, inputValue.joystick_y * playerSpeed * Time.deltaTime);
+        Player.transform.eulerAngles = new Vector3(0f, Mathf.Atan2(inputValue.joystick_x, inputValue.joystick_y) * Mathf.Rad2Deg, 0f);
+
+        Player.transform.position += move;
+
     }
+
+
 }
+
---
+++ Unity/PetEver/Assets/02.Scripts/PlayerInput.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+// reference : https://tenlie10.tistory.com/115
+public class PlayerInput : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    private Image JoystickBGD;
+    private Image JoystickPad;
+    private Vector3 inputVector;
+
+    //assign player's input value for property
+    public float joystick_x { get; private set; } // 
+    public float joystick_y { get; private set; } // 
+
+
+    void Start()
+    {
+        JoystickBGD = GetComponent<Image>();
+        JoystickPad = transform.GetChild(0).GetComponent<Image>();
+
+    }
+
+    private void Update()
+    {
+        joystick_x = inputVector.x;
+        joystick_y = inputVector.y;
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(JoystickBGD.rectTransform, eventData.position, eventData.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / JoystickBGD.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / JoystickBGD.rectTransform.sizeDelta.y);
+            inputVector = new Vector3(pos.x * 2, pos.y * 2, 0);
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+            // joystick control pad moving by user's drag
+            JoystickPad.rectTransform.anchoredPosition = new Vector3(inputVector.x * (JoystickBGD.rectTransform.sizeDelta.x / 3)
+                                                                     , inputVector.y * (JoystickBGD.rectTransform.sizeDelta.y / 3));
+
+        }
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        // reset JoystickPad position 
+        inputVector = Vector3.zero;
+        JoystickPad.rectTransform.anchoredPosition = Vector3.zero;
+    }
+
+    public float GetHorizontalValue()
+    {
+        return inputVector.x;
+    }
+
+    public float GetVerticalValue()
+    {
+        return inputVector.y;
+    }
+}
+
