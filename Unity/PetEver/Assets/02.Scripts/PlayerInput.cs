ddaa701747674f0ee1ce3f286904fd2187299ed5
jungining <jungin641@gmail.com>
2022-12-29 11:50:23 +0900

    bug fixture : after load new scene

--- Unity/PetEver/Assets/02.Scripts/PlayerInput.cs
+++ Unity/PetEver/Assets/02.Scripts/PlayerInput.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 
 // reference : https://tenlie10.tistory.com/115
 public class PlayerInput : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
