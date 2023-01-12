ba8d17a61e3badd66069d041bad3aef7a48edda9
jungining <jungin641@gmail.com>
2023-01-12 17:15:00 +0900

    DEL logs for debug

--- Unity/PetEver/Assets/02.Scripts/EnterHome.cs
+++ Unity/PetEver/Assets/02.Scripts/EnterHome.cs
@@ -26,11 +26,12 @@ public class EnterHome : MonoBehaviour
 
     IEnumerator<object> LoadYourAsyncScene()
     {
+        string sceneName = "newScene";
         // Set the current Scene to be able to unload it later
         Scene currentScene = SceneManager.GetActiveScene();
  
         // The Application loads the Scene in the background at the same time as the current Scene.
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("newScene", LoadSceneMode.Additive);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
  
         // Wait until the last operation fully loads to return anything
         while (!asyncLoad.isDone)
@@ -40,9 +41,9 @@ public class EnterHome : MonoBehaviour
 
  
         // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
-        SceneManager.MoveGameObjectToScene(ManCharacter, SceneManager.GetSceneByName("newScene"));
-        SceneManager.MoveGameObjectToScene(MainEvent, SceneManager.GetSceneByName("newScene"));
-        SceneManager.MoveGameObjectToScene(MainCanvas, SceneManager.GetSceneByName("newScene"));
+        SceneManager.MoveGameObjectToScene(ManCharacter, SceneManager.GetSceneByName(sceneName));
+        SceneManager.MoveGameObjectToScene(MainEvent, SceneManager.GetSceneByName(sceneName));
+        SceneManager.MoveGameObjectToScene(MainCanvas, SceneManager.GetSceneByName(sceneName));
         // Unload the previous Scene
         SceneManager.UnloadSceneAsync(currentScene);
     }
