f941f1ef36bffdefc4e88479058c3e69a6106cf8
jungining <jungin641@gmail.com>
2023-01-25 15:22:22 +0900

    ADD mobile touch camera asset

---
+++ Unity/PetEver/Assets/mobile_touch_camera/scripts/BoundaryMode.cs
@@ -0,0 +1,19 @@
+﻿// /************************************************************
+// *                                                           *
+// *   Mobile Touch Camera                                     *
+// *                                                           *
+// *   Created 2015 by BitBender Games                         *
+// *                                                           *
+// *   bitbendergames@gmail.com                                *
+// *                                                           *
+// ************************************************************/
+
+namespace BitBenderGames {
+
+  public enum BoundaryMode {
+    DEFAULT, //This mode tries to ensure that the camera can never see what's behind the boundary line.
+    CLAMP_POSITION, //This mode just clamps the position of the camera to the given boundary values.
+                    //Depending on the zoom and tilt the camera will be able to see behind the boundary lines but not move beyond them.
+                    //This mode should be used when creating a google-earth-like camera that can have low tilt angles.
+  }
+}
---
+++ Unity/PetEver/Assets/mobile_touch_camera/scripts/tools/DoubleClickZoom.cs
@@ -0,0 +1,51 @@
+﻿// /************************************************************
+// *                                                           *
+// *   Mobile Touch Camera                                     *
+// *                                                           *
+// *   Created 2015 by BitBender Games                         *
+// *                                                           *
+// *   bitbendergames@gmail.com                                *
+// *                                                           *
+// ************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+namespace BitBenderGames {
+
+  /// <summary>
+  /// Helper script that adds Double Click To Zoom functionality to Mobile Touch Camera.
+  /// 
+  /// Usage: Add this component to the GameObject that also has the MobileTouchCamera
+  ///        and the TouchInputController attached.
+  /// </summary>
+  [RequireComponent(typeof(MobileTouchCamera))]
+  [RequireComponent(typeof(TouchInputController))]
+  public class DoubleClickZoom : MonoBehaviour {
+
+    [SerializeField]
+    private float zoomAmount = 5f;
+
+    private MobileTouchCamera mobileTouchCamera;
+
+    private TouchInputController touchInputController;
+
+    private void Awake() {
+      mobileTouchCamera = GetComponent<MobileTouchCamera>();
+      touchInputController = GetComponent<TouchInputController>();
+      touchInputController.OnInputClick += TouchInputController_OnInputClick;
+    }
+
+    private void TouchInputController_OnInputClick(Vector3 clickPosition, bool isDoubleClick, bool isLongTap) {
+      if(isDoubleClick == true) {
+        OnDoubleClick();
+      }
+    }
+
+    public void OnDoubleClick() {
+      var zoom = mobileTouchCamera.CamZoom;
+      var zoomTarget = zoom - zoomAmount;
+      StartCoroutine(mobileTouchCamera.ZoomToTargetValueCoroutine(zoomTarget));
+    }
+  }
+}
