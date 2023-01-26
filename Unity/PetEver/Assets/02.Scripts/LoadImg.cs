d09465847680c00d9a335631c1c3d629e191b24b
jeongchanKim <jc_.kim@samsung.com>
2023-01-26 11:00:26 +0900

    Unity/PetEver/TherapyForest : Fix the charater rotation
    1. Change the character to see his own way
    2. Show the sample Pome image when walking the forest
    
    Signed-off-by: jeongchanKim <jc_.kim@samsung.com>

---
+++ Unity/PetEver/Assets/02.Scripts/LoadImg.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using System.IO;
+
+
+public class LoadImg : MonoBehaviour
+{
+    // void UpdateImage(string path)
+    // {
+
+    //     Texture2D tex = loadImage(image.rectTransform.sizeDelta, path);
+
+    //     // texture2d로 sprite 생성
+    //     image.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+    // }
+
+    // // 이미지 파일을 texture2d로 변환
+    // private static Texture2D loadImage(Vector2 size, string filePath)
+    // {
+       
+    //     byte[] bytes = File.ReadAllBytes(filePath);
+    //     Texture2D texture = new Texture2D((int)size.x, (int)size.y, TextureFormat.RGB24, false);
+    //     texture.filterMode = FilterMode.Trilinear;
+    //     texture.LoadImage(bytes);
+
+    //     return texture;
+    // }
+
+    // private void callbackForGallery(string path)
+    // {
+    //       UpdateImage(path);
+    // }
+
+    // void ONCLICK()
+    // {
+    //     NativeGallery.GetImageFromGallery(callbackForGallery);
+    // }
+    GameObject myImage;
+    public RawImage rawImage;
+
+    public void GetImage()
+    {
+        NativeGallery.GetImageFromGallery((image) =>  //mobile gallery folder open using NativeGallery Plugin
+        {
+            FileInfo selectedImage = new FileInfo(image); //choose image from gallery folder
+
+        /* set a limit on volume of picture
+        if (selectedImage.Length > 50000000)  
+        {
+            return;
+        }
+        */
+            if (!string.IsNullOrEmpty(image)) // if image is selected, start coroutine(load image)
+            StartCoroutine(LoadImage(image));
+
+        });
+    }
+
+    //image load coroutine            
+    IEnumerator LoadImage(string imagePath)
+    {
+        byte[] imageData = File.ReadAllBytes(imagePath); // read file and put in byte array
+        string imageName = Path.GetFileName(imagePath).Split('.')[0]; // save image name except image extension
+        string saveImagePath = Application.persistentDataPath + "/Image"; // save data path in image folder 
+                                                                          // for the first time, get image from gallery and next, get from folder
+
+        if (Directory.Exists(saveImagePath)) // if file to save image is not exist, make path first
+        {
+            Directory.CreateDirectory(saveImagePath);
+        }
+
+        File.WriteAllBytes(saveImagePath + imageName + ".jpg", imageData); // set path and file name to save image
+
+        var tempImage = File.ReadAllBytes(imagePath);
+
+        Texture2D texture = new Texture2D(1080, 1440);
+        texture.LoadImage(tempImage); // transfer byte array to texture 2D
+
+        rawImage.texture = texture;
+
+        yield return null;
+    }
+
+    public void showMyImage()
+    {
+        Debug.Log("Show My Image!");
+        GetImage();
+        myImage.GetComponent<Image>().sprite = Sprite.Create(rawImage.texture as Texture2D, new Rect(0, 0, rawImage.texture.width, rawImage.texture.height), new Vector2(0.5f, 0.5f));
+    } 
+
+    void Start()
+    {
+        myImage = GameObject.Find("MyImage1");
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Unity/PetEver/Assets/02.Scripts/WalkForest.cs
+++ Unity/PetEver/Assets/02.Scripts/WalkForest.cs
@@ -5,6 +5,11 @@ using System;
 using System.Diagnostics;
 using UnityEngine.UI;
 
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.Android;
+using TMPro;
+
 public class WalkForest : MonoBehaviour
 {
     GameObject forestLand;
@@ -16,9 +21,12 @@ public class WalkForest : MonoBehaviour
     public Transform Target;
     public float forestWalkSpeed = 0.8f;
 
+    int steps;
+
     void Start()
     {
         forestLand = GameObject.Find("forestLand");
+
         Input.gyro.enabled = true;
         PlayerController.isForest = true;
         manCharacter = GameObject.FindGameObjectWithTag("Owner");
@@ -26,14 +34,38 @@ public class WalkForest : MonoBehaviour
         manLookAt = GameObject.Find("ManLookAt").transform;
         forestMainCam_tr = GameObject.Find("ForestMainCam").transform;
 
+        InputSystem.AddDevice<StepCounter>();
+        if (StepCounter.current != null) {
+            InputSystem.EnableDevice(StepCounter.current);
+            StepCounter.current.MakeCurrent();
+        }
+        InputSystem.AddDevice<Accelerometer>();
+        if (Accelerometer.current != null) {
+            InputSystem.EnableDevice(Accelerometer.current);
+        }
+        steps = 0;
+    }
+
+    void WalkRound() {
         manCharacter.transform.LookAt(manLookAt);
+        manLookAt.transform.RotateAround(Target.position, Vector3.up, forestWalkSpeed * Time.deltaTime);
+        manCharacter.transform.RotateAround(Target.position, Vector3.up, forestWalkSpeed * Time.deltaTime);
+        forestMainCam_tr.RotateAround(Target.position, Vector3.up, forestWalkSpeed * Time.deltaTime);
+        PlayerController.playerAnimator.SetFloat("walking", forestWalkSpeed);
     }
 
     void Update()
     {
-        manCharacter.transform.RotateAround(Target.position, Vector3.up, forestWalkSpeed * Time.deltaTime);
-        forestMainCam_tr.RotateAround(Target.position, Vector3.up, forestWalkSpeed * Time.deltaTime);
-        PlayerController.playerAnimator.SetFloat("walking", forestWalkSpeed);
+        if (StepCounter.current != null && StepCounter.current.enabled) {
+            if (StepCounter.current.stepCounter.ReadValue() > steps) {
+                steps = StepCounter.current.stepCounter.ReadValue();   
+            }
+        } else {
+            print("No step counter found!");
+        }
+
+        WalkRound();
+
         //transform.Rotate(Input.gyro.rotationRateUnbiased.x, Input.gyro.rotationRateUnbiased.y, Input.gyro.rotationRateUnbiased.z);
         // UnityEngine.Debug.Log("x: " + Input.gyro.rotationRateUnbiased.x + " y: " + Input.gyro.rotationRateUnbiased.y + " z: " + Input.gyro.rotationRateUnbiased.z);
         if (Math.Abs(Input.gyro.rotationRateUnbiased.x) > 0.3f && Math.Abs(Input.gyro.rotationRateUnbiased.y) > 0.3f && Math.Abs(Input.gyro.rotationRateUnbiased.z) > 0.3f) {
