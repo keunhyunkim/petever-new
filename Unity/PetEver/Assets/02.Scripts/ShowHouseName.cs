a8598b45a5d5fd020d75031dcc8d20ecd22679e9
jeongchanKim <jc_.kim@samsung.com>
2023-01-31 14:32:58 +0900

    Unity/PetEver : Add SpeechBubble
    To create speechbubble, add shader, material and sample speech-bubble.
    
    Signed-off-by: jeongchanKim <jc_.kim@samsung.com>

---
+++ Unity/PetEver/Assets/02.Scripts/ShowHouseName.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowHouseName : MonoBehaviour
+{
+    // GameObject bubble;
+    // Text bubbleTxt;
+    // void Start() {
+    //     bubble = GameObject.FindGameObjectWithTag("SpeechBubble");
+    //     bubbleTxt = GameObject.Find("BubbleText").GetComponent<Text>();
+    // }
+
+    // private void OnCollisionEnter(Collision collision)
+    // {
+    //     bubbleTxt.text = "병원";
+    //     bubble.SetActive(true);
+    // }
+
+    // private void OnCollisionExit(Collision collision)
+    // {
+    //     bubble.SetActive(false);
+    // }
+}
