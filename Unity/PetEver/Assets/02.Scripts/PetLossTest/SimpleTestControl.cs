eeb557794ea5735c180decbffac6d40b8fb3b1bd
김정찬/C-Lab/삼성전자 <jc_.kim@samsung.com>
2023-03-03 11:05:02 +0900

    Unity/SimpleTest : Change the answers to button
    1) Change the answers to button and add its onclick handler
      Define 5 question groups and adding 4 answers into each group
    2) Calculate the pet-loss score using selected answer
    
    Signed-off-by: 김정찬/C-Lab/삼성전자 <jc_.kim@samsung.com>

---
+++ Unity/PetEver/Assets/02.Scripts/PetLossTest/SimpleTestControl.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTestControl : MonoBehaviour
+{
+
+    public GameObject canvasPrefab;
+    GameObject previousCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Owner")
+        {
+            previousCanvas = GameObject.FindGameObjectWithTag("SimpleTest");
+            if (previousCanvas == null)
+            {
+                GameObject canvas = Instantiate(canvasPrefab) as GameObject;
+            }
+        }
+
+    }
+
+    public void OnExitClicked()
+    {
+        // Debug.Log(SimpleTest.totalScore);
+        
+        // Initialize the totalScore
+        SimpleTest.totalScore = SimpleTest.DEFAULT_TOTALSCORE;
+        previousCanvas = GameObject.FindGameObjectWithTag("SimpleTest");
+        previousCanvas.SetActive(false);
+    }
+}
