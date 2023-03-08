a02b99dec184b390a7c1dc72474c7feef2100fc9
김정찬/C-Lab/삼성전자 <jc_.kim@samsung.com>
2023-03-08 17:10:55 +0900

    Unity/PetLossTest : Change to show PetLoss Test
    Signed-off-by: 김정찬/C-Lab/삼성전자 <jc_.kim@samsung.com>

--- Unity/PetEver/Assets/02.Scripts/PetLossTest/PetlossTestStart.cs
+++ Unity/PetEver/Assets/02.Scripts/PetLossTest/PetlossTestStart.cs
@@ -5,14 +5,56 @@ using UnityEngine;
 public class PetlossTestStart : MonoBehaviour
 {
     Vector3 StartPoint;
+    public GameObject petlossTestPrefab;
+    CanvasGroup testCanvas;
+    CanvasGroup chatNnoti;
+
+
+    void Start()
+    {
+        testCanvas = GameObject.Find("PetLossTestPannel").GetComponent<CanvasGroup>();
+        chatNnoti = GameObject.Find("ChatAndNoti").GetComponent<CanvasGroup>();
+    }
+
+    private void showPetLossTest()
+    {
+        testCanvas.alpha = 1;
+        testCanvas.interactable = true;
+        testCanvas.blocksRaycasts = true;
+
+        chatNnoti.alpha = 0;
+        chatNnoti.interactable = false;
+        chatNnoti.blocksRaycasts = false;
+    }
+
+    private void hidePetLossTest()
+    {
+        testCanvas.alpha = 0;
+        testCanvas.interactable = false;
+        testCanvas.blocksRaycasts = false;
+
+        chatNnoti.alpha = 1;
+        chatNnoti.interactable = true;
+        chatNnoti.blocksRaycasts = true;
+    }
+
+    private void hideGuide() {
+        GameObject.Find("PetLossTestGuide").SetActive(false);
+    }
+
+    public void startPetlossTest()
+    {
+        // StartPoint = GameObject.Find("TestPosition").transform.position;
+        // collision.transform.position = StartPoint;
+
+        //Hide the Guide after 2 seconds
+        Invoke("hideGuide", 2.0f);
+
+        showPetLossTest();
+    }
 
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.tag == "Owner")
-        {
-            StartPoint = GameObject.Find("TestPosition").transform.position;
-            collision.transform.position = StartPoint;
-        }
 
     }
 }
--- Unity/PetEver/Assets/02.Scripts/PetLossTest/SimpleTestControl.cs
+++ Unity/PetEver/Assets/02.Scripts/PetLossTest/SimpleTestControl.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class SimpleTestControl : MonoBehaviour
 {
+    CanvasGroup testCanvas;
+    CanvasGroup testGuide;
+    CanvasGroup chatNnoti;
+
+    void Start()
+    {
+        testCanvas = GameObject.Find("PetLossTestPannel").GetComponent<CanvasGroup>();
+        testGuide = GameObject.Find("PetLossTestGuide").GetComponent<CanvasGroup>();
+        chatNnoti = GameObject.Find("ChatAndNoti").GetComponent<CanvasGroup>();
+    }
+
+    private void showTestGuide()
+    {
+        testGuide.alpha = 1;
+        testGuide.interactable = true;
+        testGuide.blocksRaycasts = true;
+    }
+
     public void OnExitClicked()
     {
         // Debug.Log(SimpleTest.totalScore);
@@ -13,5 +31,45 @@ public class SimpleTestControl : MonoBehaviour
         GameObject previousCanvas = GameObject.FindGameObjectWithTag("SimpleTest");
         Destroy(previousCanvas);
         HealingGuide.OnSimpleTestExitClicked();
+        
+        showTestGuide();
+        Invoke("showPetLossTest", 3.0f);
+        
+    }
+
+
+
+    private void startPetlossTest()
+    {
+        // StartPoint = GameObject.Find("TestPosition").transform.position;
+        // collision.transform.position = StartPoint;
+
+        showPetLossTest();
+    }
+
+    private void showPetLossTest()
+    {
+        testCanvas.alpha = 1;
+        testCanvas.interactable = true;
+        testCanvas.blocksRaycasts = true;
+
+        chatNnoti.alpha = 0;
+        chatNnoti.interactable = false;
+        chatNnoti.blocksRaycasts = false;
+    }
+
+    private void hidePetLossTest()
+    {
+        testCanvas.alpha = 0;
+        testCanvas.interactable = false;
+        testCanvas.blocksRaycasts = false;
+
+        chatNnoti.alpha = 1;
+        chatNnoti.interactable = true;
+        chatNnoti.blocksRaycasts = true;
+    }
+
+    private void hideGuide() {
+        GameObject.Find("PetLossTestGuide").SetActive(false);
     }
 }
