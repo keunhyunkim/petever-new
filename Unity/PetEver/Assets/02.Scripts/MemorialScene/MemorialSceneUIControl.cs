d44dec77791933559638f05dc1b9e2c878de1302
jungining <jungin641@gmail.com>
2023-03-24 11:04:43 +0900

    ADD attaching post-it on the wall in Memorial space

--- Unity/PetEver/Assets/02.Scripts/MemorialScene/MemorialSceneUIControl.cs
+++ Unity/PetEver/Assets/02.Scripts/MemorialScene/MemorialSceneUIControl.cs
@@ -7,13 +7,17 @@ public class MemorialSceneUIControl : MonoBehaviour
 {
     CanvasGroup stickyNoteInputPanel;
     CanvasGroup photoPanel;
-    TMP_InputField stickyNoteInput;
+    TMP_InputField stickyNoteInput;    
+    string resourceUrl = "Prefabs/";
+    GameObject owner;
+    GameObject wallArea;
     void Start()
     {
         stickyNoteInputPanel = GameObject.Find("StickyNotePanel").GetComponent<CanvasGroup>();
         photoPanel = GameObject.Find("PhotoPanel").GetComponent<CanvasGroup>();
         stickyNoteInput = GameObject.Find("StickyNoteInput").GetComponent<TMP_InputField>();
-
+        owner = GameObject.FindGameObjectWithTag("Owner");
+        wallArea = GameObject.Find("Wall Area");
     }
     
     public void OnExitClicked()
@@ -27,8 +31,27 @@ public class MemorialSceneUIControl : MonoBehaviour
     public void OnCompleteClicked()
     {
         // TODO :  save input text into server..
-        
+        createPostIt();
         hideCanvasGroup(stickyNoteInputPanel);
+    }
+
+    void createPostIt(){
+        GameObject postItPrefab = Resources.Load<GameObject>(resourceUrl + "postit");
+       
+        Vector3 forwardPos =  owner.transform.position + (owner.transform.forward * 3);
+        Vector3 prefabPos = postItPrefab.transform.position;
+        prefabPos.x = forwardPos.x;
+
+        
+        GameObject postIt = Instantiate(postItPrefab, prefabPos, postItPrefab.transform.rotation) as GameObject;
+
+        Transform parent = TransformExtension.FindChildByRecursive(wallArea.transform, "PostIt");
+
+        if (parent != null)
+        {
+            postIt.transform.SetParent(parent, false);
+        }
+
     }
     void hideCanvasGroup(CanvasGroup cg)
     {
