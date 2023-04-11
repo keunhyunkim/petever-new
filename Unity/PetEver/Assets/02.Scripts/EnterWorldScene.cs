266ca356a05bec4eddb0a9edde34c030fa511861
jungining <jungin641@gmail.com>
2023-04-11 16:26:30 +0900

    Set Android Export Build Config

---
+++ Unity/PetEver/Assets/02.Scripts/EnterWorldScene.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+public class EnterWorldScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClickContinue()
+    {
+        StartCoroutine(LoadYourAsyncScene());
+    }
+
+    IEnumerator<object> LoadYourAsyncScene()
+    {
+        string sceneName = "MemorialSpace";
+        // Set the current Scene to be able to unload it later
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // The Application loads the Scene in the background at the same time as the current Scene.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+        // Wait until the last operation fully loads to return anything
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+    }
+
+    public void OnClickRemake()
+    {
+        Application.Quit();
+    }
+
+
+
+}
