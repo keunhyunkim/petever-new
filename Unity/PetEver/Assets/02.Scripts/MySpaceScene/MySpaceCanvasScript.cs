06ca2bd5f0a25242a91eb71f39194b7a66886b57
jungining <jungin641@gmail.com>
2023-04-26 17:23:50 +0900

    ADD WorldScenePopup to MySpaceScene

--- Unity/PetEver/Assets/02.Scripts/MySpaceScene/MySpaceCanvasScript.cs
+++ Unity/PetEver/Assets/02.Scripts/MySpaceScene/MySpaceCanvasScript.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MySpaceCanvasScript : MonoBehaviour
 {
 
     public CanvasGroup questPopupPanel;
     public CanvasGroup questDetailPopup;
+    public CanvasGroup worldScenePopup;
     public GameObject questSelectionLight;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -21,21 +22,55 @@ public class MySpaceCanvasScript : MonoBehaviour
 
     }
 
-    public void onClickQuestBtn(){
+    public void onClickQuestBtn()
+    {
         questSelectionLight.SetActive(true);
         showCanvasGroup(questPopupPanel);
     }
-    public void onClickCloseBtn(){
+    public void onClickCloseBtn()
+    {
         hideCanvasGroup(questPopupPanel);
         questSelectionLight.SetActive(false);
     }
-    public void onClickQuestItem(){
+    public void onClickQuestItem()
+    {
         showCanvasGroup(questDetailPopup);
     }
-    public void onClickQuestDetailComplete(){
+    public void onClickQuestDetailComplete()
+    {
         hideCanvasGroup(questDetailPopup);
     }
+    public void onClickGoToWorldScene()
+    {
+        StartCoroutine(LoadYourAsyncScene());
+    }
+    public void onClickWorldScene()
+    {
+        showCanvasGroup(worldScenePopup);
+    }
+    public void onClickCloseWorldScenePopupBtn()
+    {
+        hideCanvasGroup(worldScenePopup);
+    }
+    IEnumerator<object> LoadYourAsyncScene()
+    {
+        string sceneName = "WorldScene";
+        // Set the current Scene to be able to unload it later
+        Scene currentScene = SceneManager.GetActiveScene();
 
+        // The Application loads the Scene in the background at the same time as the current Scene.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+        // Wait until the last operation fully loads to return anything
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+    }
     void showCanvasGroup(CanvasGroup cg)
     {
         cg.alpha = 1;
