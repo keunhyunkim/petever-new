183faf2132347ae67a68aac41fe40f7d202b4be8
keunhyunkim <keunhyun12@gmail.com>
2023-05-08 15:08:20 +0900

    Add Dog Prefab - Add dog prefabs, materials, actions

---
+++ Unity/PetEver/Assets/02.Scripts/Characteristic/DogInteraction.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogInteraction : MonoBehaviour
+{
+    public GameObject DogCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DogCamera = GameObject.Find("Camera");
+    }
+
+    // Update is called once per frame
+    // https://ssscool.tistory.com/336
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            // 싱글 터치.
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPos;
+            Ray ray;
+            RaycastHit hit;
+ 
+            switch (touch.phase)
+            {
+                case TouchPhase.Began: // when touch starts
+                    Debug.Log(touch.position.x);
+                    Debug.Log(touch.position.y);
+
+                    Vector3 touchPosToVector3 = new Vector3(touch.position.x,touch.position.y,-900);
+                    //touchPos = DogCamera.GetComponent<Camera>().ScreenToWorldPoint(touchPosToVector3);
+                    ray = DogCamera.GetComponent<Camera>().ScreenPointToRay(touchPosToVector3); 
+                    if (Physics.Raycast(ray,out hit))
+                    {
+                        Debug.DrawLine(ray.origin, hit.point, Color.red, 10.5f);
+                        Debug.Log("ok"); 
+                        if(hit.collider.gameObject.name == "footTouch_R")
+                        {
+                            Debug.Log("R");
+                        }
+                        else if(hit.collider.gameObject.name == "footTouch_L")
+                        {
+                            Debug.Log("L");
+                        }
+                    }
+                    else
+                    {                        
+                        Debug.DrawLine(ray.origin, hit.point, Color.blue, 10.5f);
+                        Debug.Log("fail");
+                    }
+ 
+                    break;
+                case TouchPhase.Moved:
+                    // 터치 이동 시.
+                    break;
+ 
+                case TouchPhase.Stationary:
+                    // 터치 고정 시.
+                    break;
+ 
+                case TouchPhase.Ended:
+                    // 터치 종료 시. ( 손을 뗐을 시 )
+                    break;
+ 
+                case TouchPhase.Canceled:
+                    // 터치 취소 시. ( 시스템에 의해서 터치가 취소된 경우 (ex: 전화가 왔을 경우 등) )
+                    break;
+            }
+        }
+    }
+}
--- Unity/PetEver/Assets/02.Scripts/Characteristic/SetCharacterFeature.cs
+++ Unity/PetEver/Assets/02.Scripts/Characteristic/SetCharacterFeature.cs
@@ -5,9 +5,30 @@ using TMPro;
 
 public class SetCharacterFeature : MonoBehaviour
 {
+    private Vector3 dogSummonPos = new Vector3(-0.21f, -1.66f, -0.24f);
+    private Vector3 dogScale = new Vector3(2f, 2f, 2f);
+
     public GameObject maltesePrefab;
     public GameObject pomeLongPrefab;
     public GameObject pomeShortPrefab;
+    public GameObject pugPrefab;
+
+    private Material dogMaterial;
+    
+    public Material pomeLong_black;
+    public Material pomeLong_brown;
+    public Material pomeLong_white;
+
+    public Material pomeShort_black;
+    public Material pomeShort_brown;
+    public Material pomeShort_white;
+
+    public Material pug_black;
+    public Material pug_brown;
+
+    public Material maltese_white;
+
+    private SkinnedMeshRenderer meshRenderer; 
 
     private String breed = "";
     private String petName = "";
@@ -17,7 +38,7 @@ public class SetCharacterFeature : MonoBehaviour
 
     void Awake()
     {
-        Vector3 dogScale = new Vector3(0.8f, 0.8f, 0.8f);
+        //Vector3 dogScale = new Vector3(2f, 2f, 2f);
 
         using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
         {
@@ -44,24 +65,55 @@ public class SetCharacterFeature : MonoBehaviour
                 Debug.Log("GameManager Exception : " + e.ToString());
             }
 
+            breed = "POME_LONG";
+            section1Color = "000000";
+
             switch (breed)
             {
                 case "POME_LONG":
                     dogPrefab = pomeLongPrefab;
+                    meshRenderer = dogPrefab.transform.Find("body").GetComponent<SkinnedMeshRenderer>();
+
+                    if ((section1Color.CompareTo("ffffff") == 0) || (section1Color.CompareTo("f0ddcf") == 0)){
+                        meshRenderer.material = pomeLong_white;
+                    }
+                    else if((section1Color.CompareTo("c68642") == 0) || (section1Color.CompareTo("945826") == 0)){
+                        meshRenderer.material = pomeLong_brown;
+                    }
+                    else if(section1Color.CompareTo("000000") == 0){
+                        meshRenderer.material = pomeLong_black;
+                    }
                     break;
                 case "POME_SHORT":
                     dogPrefab = pomeShortPrefab;
+                    meshRenderer = dogPrefab.transform.Find("body").GetComponent<SkinnedMeshRenderer>();
+
+                    if ((section1Color.CompareTo("ffffff") == 0) || (section1Color.CompareTo("f0ddcf") == 0)){
+                        meshRenderer.material = pomeShort_white;
+                    }
+                    else if((section1Color.CompareTo("c68642") == 0) || (section1Color.CompareTo("945826" )==0)){
+                        meshRenderer.material = pomeShort_brown;
+                    }
+                    else if(section1Color.CompareTo("000000") == 0){
+                        meshRenderer.material = pomeShort_black;
+                    }
                     break;
                 case "MALTESE":
                     dogPrefab = maltesePrefab;
+                    break;
+                case "PUG":
+                    dogPrefab = pugPrefab;
                     break;
                 default:
                     dogPrefab = pomeShortPrefab;
                     break;
             }
 
-            GameObject dog = Instantiate(dogPrefab, GameObject.Find("Object Parent").transform) as GameObject;
+
+            GameObject dog = Instantiate(dogPrefab, GameObject.Find("Character").transform) as GameObject;
             dog.transform.localScale = dogScale;
+            dog.transform.localPosition = dogSummonPos;
+
         }
     }
 }
