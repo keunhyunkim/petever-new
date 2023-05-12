d5ad8872b8f53e7c01661a1b9af5fe941b923bf8
keunhyunkim <keunhyun12@gmail.com>
2023-05-12 15:34:35 +0900

    ColorChangeTest-ing

---
+++ Unity/PetEver/Assets/02.Scripts/Characteristic/TextureColorChange.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureColorChange : MonoBehaviour
+{
+   // public Texture2D originTexture;
+   // private Color originColor;
+  //  private Color newColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  originColor = (0.702, 0.498, 0.275, 1);
+     //   newColor = (0.593, 0.421, 0.232, 1);
+       // ColorChange();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ColorChange()
+    {
+        /*
+        var colors = originTexture.GetPixels();
+        
+        for(int i=0; i<100; i++)
+        {
+            Debug.Log(colors[i]);
+            if(colors[i] == originColor)
+            {
+                Debug.Log("correct");
+                colors[i]=newColor;
+            }
+        }
+        originTexture.SetPixels(colors);
+        originTexture.Apply();
+        */
+    }
+
+}
