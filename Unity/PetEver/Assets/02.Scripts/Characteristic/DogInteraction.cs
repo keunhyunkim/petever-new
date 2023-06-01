4908ed667fb68e54684100ed8943bca99695014d
jungining <jungin641@gmail.com>
2023-06-01 16:02:43 +0900

    Add Feeding Interaction

--- Unity/PetEver/Assets/02.Scripts/Characteristic/DogInteraction.cs
+++ Unity/PetEver/Assets/02.Scripts/Characteristic/DogInteraction.cs
@@ -10,6 +10,7 @@ public class DogInteraction : MonoBehaviour
     public GameObject touchtracking;
     private float time_start;
     private float reset_flag;
+    [SerializeField] private GameObject MeatPrefab;
 
 
     // Start is called before the first frame update
@@ -24,7 +25,7 @@ public class DogInteraction : MonoBehaviour
     // Update is called once per frame
     // https://ssscool.tistory.com/336
     void Update()
-    {   
+    {
         if (Input.touchCount > 0)
         {
             // 싱글 터치.
@@ -35,33 +36,28 @@ public class DogInteraction : MonoBehaviour
             RaycastHit hit;
             Vector3 touchPosToVector3;
 
- 
+
             switch (touch.phase)
             {
                 case TouchPhase.Began: // when touch starts
 
 
-                    touchPosToVector3 = new Vector3(touch.position.x,touch.position.y, 0);
+                    touchPosToVector3 = new Vector3(touch.position.x, touch.position.y, 0);
                     //touchPos = Camera.main.GetComponent<Camera>().ScreenToWorldPoint(touchPosToVector3);
                     ray = DogCamera.GetComponent<Camera>().ScreenPointToRay(touchPosToVector3);
                     //ray_main = Camera.main.GetComponent<Camera>().ScreenPointToRay(touchPosToVector3);
                     //Debug.DrawLine(ray.origin,touchPos, Color.yellow, 1.5f); 
-                    if (Physics.Raycast(ray,out hit))
+                    if (Physics.Raycast(ray, out hit))
                     {
-                        if(hit.collider.gameObject.name == "footTouch_R")
+                        if (hit.collider.gameObject.name == "footTouch_R")
                         {
                             dogAnimator.SetTrigger("feetup_L");
                         }
-                        else if(hit.collider.gameObject.name == "footTouch_L")
+                        else if (hit.collider.gameObject.name == "footTouch_L")
                         {
                             dogAnimator.SetTrigger("feetup_R");
                         }
                     }
-                    else
-                    {                        
-
-                    }
- 
                     break;
                 case TouchPhase.Moved:
                     if (reset_flag == 0)
@@ -69,25 +65,25 @@ public class DogInteraction : MonoBehaviour
                         time_start = Time.time;
                     }
                     reset_flag = 1;
-                    touchPosToVector3 = new Vector3(touch.position.x,touch.position.y, 0);
-                    ray = DogCamera.GetComponent<Camera>().ScreenPointToRay(touchPosToVector3); 
-                    if (Physics.Raycast(ray,out hit))
+                    touchPosToVector3 = new Vector3(touch.position.x, touch.position.y, 0);
+                    ray = DogCamera.GetComponent<Camera>().ScreenPointToRay(touchPosToVector3);
+                    if (Physics.Raycast(ray, out hit))
                     {
-                        if((hit.collider.gameObject.name == "headTouch") && (Time.time-time_start) > 0.5f)
+                        if ((hit.collider.gameObject.name == "headTouch") && (Time.time - time_start) > 0.5f)
                         {
                             dogAnimator.SetTrigger("heading");
                         }
                     }
                     break;
- 
+
                 case TouchPhase.Stationary:
                     // 터치 고정 시.
                     break;
- 
+
                 case TouchPhase.Ended:
                     reset_flag = 0;
                     break;
- 
+
                 case TouchPhase.Canceled:
                     // 터치 취소 시. ( 시스템에 의해서 터치가 취소된 경우 (ex: 전화가 왔을 경우 등) )
                     break;
@@ -99,4 +95,14 @@ public class DogInteraction : MonoBehaviour
     {
         dogAnimator.SetTrigger("uniqueMotion");
     }
+    public void Feeding()
+    {
+        dogAnimator.SetTrigger("snuffing");
+        if (MeatPrefab != null)
+        {
+            GameObject meat = Instantiate(MeatPrefab);
+            Object.Destroy(meat, 6.0f);
+        }
+    }
+
 }
